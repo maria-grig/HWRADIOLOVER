c7e62697eae5574ac4c9d12c2c6a53b65741e761
MariaG <MGrigorenko@bellintegrator.com>
2022-11-05 17:27:50 +0300

    combined the separated classes into one class

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -1,55 +1,73 @@
 package ru.netology.radio;
-
 public class Radio {
-    private int currentStation;
+
+    private int minStation;
     private int currentVolume;
+    private int maxStation;
+    private int totalRadioStations;
+    private int currentStation = minStation;
+
+    public Radio(int totalRadioStations) {
+        this.totalRadioStations = totalRadioStations;
+        this.maxStation = totalRadioStations - 1;
+    }
+
+    public Radio() {
+        this.totalRadioStations = 10;
+        this.maxStation = totalRadioStations - 1;
+    }
 
     public int getCurrentStation() {
         return currentStation;
     }
 
     public void setCurrentStation(int currentStation) {
-        if ((currentStation >= 0) && (currentStation <= 9)) {
+        if (currentStation >= minStation && currentStation <= maxStation) {
             this.currentStation = currentStation;
+        } else {
+            return;
         }
     }
 
-    public void nextStation() {
-        if (currentStation == 9) {
-            currentStation = 0;
-            return;
+    public void NextStation() {
+        if (currentStation == maxStation) {
+            this.currentStation = 0;
+        } else {
+            this.currentStation = currentStation + 1;
         }
-        currentStation++;
     }
 
     public void prevStation() {
-        if (currentStation == 0) {
-            currentStation = 9;
+        if (currentStation == minStation) {
+            this.currentStation = maxStation;
+        } else {
+            this.currentStation = currentStation - 1;
             return;
         }
         currentStation--;
     }
-
     public int getCurrentVolume() {
         return currentVolume;
     }
 
     public void setCurrentVolume(int currentVolume) {
-        this.currentVolume = currentVolume;
-    }
-
-    public void increaseVolume() {
-        if (currentVolume == 10) {
+        if (currentVolume >= 0 && currentVolume <= 100) {
+            this.currentVolume = currentVolume;
+        } else {
             return;
         }
-        currentVolume++;
     }
 
-    public void decreaseVolume() {
-        if (currentVolume == 0) {
-            return;
+    public void IncreaseVolume() {
+        if (currentVolume < 100) {
+            this.currentVolume = currentVolume + 1;
         }
-        currentVolume--;
+    }
+
+    public void DecreaseVolume() {
+        if (currentVolume > 0) {
+            this.currentVolume = currentVolume - 1;
+        }
     }
 
 }
--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -1,130 +1,270 @@
 package ru.netology.radio;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RadioTest {
-
-    @Test
-    void shouldNotAcceptLessThenPossible() {
+    @Test // set the current station
+    public void shouldSetTheCurrentStation(){
         Radio service = new Radio();
-
-        int wrongStationNumber = -1;
-        service.setCurrentStation(wrongStationNumber);
-
-        int expected = 0;
-
-        assertEquals(expected, service.getCurrentStation());
+        service.setCurrentStation(5);
+        Assertions.assertEquals(5, service.getCurrentStation());
     }
 
-    @Test
-    void shouldNotAcceptMoreThenPossible() {
+    @Test// default number of stations, regular positive case to switch stations
+    public void shouldSwitchToNextStationTestOne() {
         Radio service = new Radio();
-
-        int wrongStationNumber = 10;
-        service.setCurrentStation(wrongStationNumber);
-
-        int expected = 0;
-
-        assertEquals(expected, service.getCurrentStation());
-    }
-
-    @Test
-    void shouldSelectNextStation() {
-        Radio service = new Radio();
-
         service.setCurrentStation(8);
-        service.nextStation();
-
-        int expected = 9;
-
-        assertEquals(expected, service.getCurrentStation());
+        service.NextStation();
+        assertEquals(9, service.getCurrentStation());
     }
 
-    @Test
-    void shouldJumpToFirstStation() {
+    @Test // default number of stations, should switch from the last station to the first station
+    public void shouldSwitchToNextStationTestTwo() {
         Radio service = new Radio();
-
         service.setCurrentStation(9);
-        service.nextStation();
-
-        int expected = 0;
-
-        assertEquals(expected, service.getCurrentStation());
+        service.NextStation();
+        assertEquals(0, service.getCurrentStation());
     }
 
-    @Test
-    void shouldSelectPrevStation() {
+    @Test // default number of stations, should switch to the first station from non-existing station
+    public void shouldSwitchToNextStationTestThree() {
         Radio service = new Radio();
+        service.setCurrentStation(10);
+        service.NextStation();
+        assertEquals(1, service.getCurrentStation());
+    }
 
+    @Test // default number of stations, should switch to the first station from non-existing station
+    public void shouldSwitchToNextStationTestFour() {
+        Radio service = new Radio();
+        service.setCurrentStation(-1);
+        service.NextStation();
+        assertEquals(1, service.getCurrentStation());
+    }
+
+    @Test // defined number of stations, should switch from the last station to the first station
+    public void shouldSwitchToNextStationTestFive() {
+        Radio service = new Radio(50);
+        service.setCurrentStation(49);
+        service.NextStation();
+        assertEquals(0, service.getCurrentStation());
+    }
+
+    @Test // defined number of stations, positive regular case
+    public void shouldSwitchToNextStationTestSix() {
+        Radio service = new Radio(50);
+        service.setCurrentStation(35);
+        service.NextStation();
+        assertEquals(36, service.getCurrentStation());
+    }
+
+    @Test // defined number of stations, switch from first station to next station
+    public void shouldSwitchToNextStationTestSeven() {
+        Radio service = new Radio(50);
+        service.setCurrentStation(35);
+        service.NextStation();
+        assertEquals(36, service.getCurrentStation());
+    }
+
+    @Test // defined number of stations, switch from non-existing station to next station
+    public void shouldSwitchToNextStationTestEight() {
+        Radio service = new Radio(50);
+        service.setCurrentStation(50);
+        service.NextStation();
+        assertEquals(1, service.getCurrentStation());
+    }
+
+    @Test // defined number of stations, switch from second to third station
+    public void shouldSwitchToNextStationTestNine() {
+        Radio service = new Radio(50);
         service.setCurrentStation(1);
-        service.prevStation();
-
-        int expected = 0;
-
-        assertEquals(expected, service.getCurrentStation());
+        service.NextStation();
+        assertEquals(2, service.getCurrentStation());
     }
 
-    @Test
-    void shouldJumpToLastStation() {
+    @Test // defined number of stations, switch from non-existing station to next station
+    public void shouldSwitchToNextStationTestTen() {
+        Radio service = new Radio(50);
+        service.setCurrentStation(-1);
+        service.NextStation();
+        assertEquals(1, service.getCurrentStation());
+    }
+    @Test // defined number of stations, switch from mid-station to next station
+    public void shouldSwitchToNextStationTestEleven() {
+        Radio service = new Radio(50);
+        service.setCurrentStation(25);
+        service.NextStation();
+        assertEquals(26, service.getCurrentStation());
+    }
+    @Test// default number of stations, regular positive case to switch stations
+    public void shouldSwitchToPrevStationTestOne() {
         Radio service = new Radio();
+        service.setCurrentStation(8);
+        service.prevStation();
+        assertEquals(7, service.getCurrentStation());
+    }
 
+    @Test// default number of stations, should switch to the previous station from the first station
+    public void shouldSwitchToPrevStationTestTwo() {
+        Radio service = new Radio();
+        service.setCurrentStation(9);
+        service.prevStation();
+        assertEquals(8, service.getCurrentStation());
+    }
+
+    @Test // default number of stations, should switch to the last station from non-existing station
+    public void shouldSwitchToPrevStationTestThree() {
+        Radio service = new Radio();
+        service.setCurrentStation(10);
+        service.prevStation();
+        assertEquals(8, service.getCurrentStation());
+    }
+    @Test // default number of stations, should switch to the last station from non-existing station
+    public void shouldSwitchToPrevStationTestFour() {
+        Radio service = new Radio();
+        service.setCurrentStation(-1);
+        service.prevStation();
+        assertEquals(8, service.getCurrentStation());
+    }
+
+    @Test // defined number of stations, should switch from the last station to the first
+    public void shouldSwitchToPrevStationTestFive() {
+        Radio service = new Radio(50);
         service.setCurrentStation(0);
         service.prevStation();
-
-        int expected = 9;
-
-        assertEquals(expected, service.getCurrentStation());
+        assertEquals(48, service.getCurrentStation());
+    }
+    @Test // defined number of stations, should switch from the first station to the last
+    public void shouldSwitchToPrevStationTestSix() {
+        Radio service = new Radio(50);
+        service.setCurrentStation(0);
+        service.prevStation();
+        assertEquals(48, service.getCurrentStation());
+    }
+    @Test // defined number of stations, should switch from the last station to previous
+    public void shouldSwitchToPrevStationTestSeven() {
+        Radio service = new Radio(50);
+        service.setCurrentStation(49);
+        service.prevStation();
+        assertEquals(48, service.getCurrentStation());
     }
 
-    @Test
-    void shouldIncreaseVolume() {
-        Radio service = new Radio();
-
-        service.setCurrentVolume(5);
-        service.increaseVolume();
-
-        int expected = 6;
-
-        assertEquals(expected, service.getCurrentVolume());
+    @Test // defined number of stations, should switch from the second station to the first station
+    public void shouldSwitchToPrevStationTestEight() {
+        Radio service = new Radio(50);
+        service.setCurrentStation(1);
+        service.prevStation();
+        assertEquals(0, service.getCurrentStation());
     }
 
-    @Test
-    void shouldNotIncreaseVolume() {
-        Radio service = new Radio();
-
-        service.setCurrentVolume(10);
-        service.increaseVolume();
-
-        int expected = 10;
-
-        assertEquals(expected, service.getCurrentVolume());
+    @Test // defined number of stations, should switch from non-existing station to the last station
+    public void shouldSwitchToPrevStationTestNine() {
+        Radio service = new Radio(50);
+        service.setCurrentStation(-1);
+        service.prevStation();
+        assertEquals(48, service.getCurrentStation());
     }
 
-    @Test
-    void shouldDecreaseVolume() {
-        Radio service = new Radio();
-
-        service.setCurrentVolume(5);
-        service.decreaseVolume();
-
-        int expected = 4;
-
-        assertEquals(expected, service.getCurrentVolume());
+    @Test // defined number of stations, should switch from non-existing station to the last station
+    public void shouldSwitchToPrevStationTestTen() {
+        Radio service = new Radio(50);
+        service.setCurrentStation(50);
+        service.prevStation();
+        assertEquals(48, service.getCurrentStation());
     }
 
-    @Test
-    void shouldNotDecreaseVolume() {
-        Radio service = new Radio();
+    @Test // defined number of stations, should switch from second last station to previous station
+    public void shouldSwitchToPrevStationTestEleven() {
+        Radio service = new Radio(50);
+        service.setCurrentStation(48);
+        service.prevStation();
+        assertEquals(47, service.getCurrentStation());
+    }
 
+    @Test // defined number of stations, should switch from mid-station to previous station
+    public void shouldSwitchToPrevStationTestTwelve() {
+        Radio service = new Radio(50);
+        service.setCurrentStation(20);
+        service.prevStation();
+        assertEquals(19, service.getCurrentStation());
+    }
+    Radio service = new Radio();
+    @Test // increase volume when volume is max
+    public void increaseVolumeTestOne() {
+        service.setCurrentVolume(100);
+        service.IncreaseVolume();
+        Assertions.assertEquals(100, service.getCurrentVolume());
+    }
+    @Test // increase volume to max
+    public void increaseVolumeTestTwo() {
+        service.setCurrentVolume(99);
+        service.IncreaseVolume();
+        Assertions.assertEquals(100, service.getCurrentVolume());
+    }
+
+    @Test // increase volume regular positive case
+    public void increaseVolumeTestThree() {
         service.setCurrentVolume(0);
-        service.decreaseVolume();
-
-        int expected = 0;
-
-        assertEquals(expected, service.getCurrentVolume());
+        service.IncreaseVolume();
+        Assertions.assertEquals(1, service.getCurrentVolume());
     }
+    @Test // increase volume from non-existing volume scale
+    public void increaseVolumeTestFour() {
+        service.setCurrentVolume(-1);
+        service.IncreaseVolume();
+        Assertions.assertEquals(1, service.getCurrentVolume());
+    }
+
+    @Test // increase volume from non-existing volume scale
+    public void increaseVolumeTestFive() {
+        service.setCurrentVolume(101);
+        service.IncreaseVolume();
+        Assertions.assertEquals(1, service.getCurrentVolume());
+    }
+
+    @Test // decrease volume from max
+    public void decreaseVolumeTestOne() {
+        service.setCurrentVolume(100);
+        service.DecreaseVolume();
+        Assertions.assertEquals(99, service.getCurrentVolume());
+    }
+
+    @Test // decrease volume from min
+    public void decreaseVolumeTestTwo() {
+        service.setCurrentVolume(0);
+        service.DecreaseVolume();
+        Assertions.assertEquals(0, service.getCurrentVolume());
+    }
+
+    @Test // decrease volume from 1 to 0
+    public void decreaseVolumeTestThree() {
+        service.setCurrentVolume(1);
+        service.DecreaseVolume();
+        Assertions.assertEquals(0, service.getCurrentVolume());
+    }
+
+    @Test // decrease volume from non-existing scale
+    public void decreaseVolumeTestFour() {
+        service.setCurrentVolume(-1);
+        service.DecreaseVolume();
+        Assertions.assertEquals(0, service.getCurrentVolume());
+    }
+
+    @Test // decrease volume from non-existing scale
+    public void decreaseVolumeTestFive() {
+        service.setCurrentVolume(101);
+        service.DecreaseVolume();
+        Assertions.assertEquals(0, service.getCurrentVolume());
+    }
+
+    @Test // decrease volume regular positive case
+    public void DecreaseVolumeTestSix() {
+        service.setCurrentVolume(17);
+        service.DecreaseVolume();
+        Assertions.assertEquals(16, service.getCurrentVolume());
+    }
+
 }
-
-
